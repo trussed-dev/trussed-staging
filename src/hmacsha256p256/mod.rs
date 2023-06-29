db6cf12bdc3e740963b952a27318362eb93310f3
Szczepan Zalega <szczepan@nitrokey.com>
2023-06-29 12:59:12 +0200

    Review: check if the target key is symmetric before use
    To avoid accidental use of other secret material

--- src/hmacsha256p256/mod.rs
+++ src/hmacsha256p256/mod.rs
@@ -11,6 +11,7 @@ use trussed::{
     types::{Bytes, CoreContext, KeyId, Location, Mechanism},
     Error,
 };
+use trussed::key::Kind::Symmetric;
 
 #[derive(Debug, Default)]
 pub struct HmacSha256P256Extension;
@@ -147,7 +148,7 @@ pub fn derive_key_from_hash(
     type HmacSha256P256 = Hmac<sha2::Sha256>;
 
     let key_id = request.key;
-    let key = keystore.load_key(key::Secrecy::Secret, None, &key_id)?;
+    let key = keystore.load_key(key::Secrecy::Secret, Some(Symmetric(32)), &key_id)?;
     let shared_secret = key.material;
 
     let mut mac =
